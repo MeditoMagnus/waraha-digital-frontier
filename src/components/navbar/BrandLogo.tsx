072ebcd356b46da6ffdd9ca444d1e71415374130
Medito Magnus <meditomagnus@gmail.com>
2025-05-26 02:26:06 +0400

    Update BrandLogo.tsx

--- src/components/navbar/BrandLogo.tsx
+++ src/components/navbar/BrandLogo.tsx
@@ -7,7 +7,7 @@ export const BrandLogo = () => {
       <img 
         src="/lovable-uploads/WhatsApp_Image_2025-05-23_at_13.46.02-removebg-preview.png" 
         alt="Waraha Group Logo" 
-        className="h-12 w-[151px]"
+        className="h-[90] w-[151px]"
 
       />
     </a>
